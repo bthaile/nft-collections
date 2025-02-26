7205c3c01f00d5ef163a29d42a85cfbe0baba716
Tom Haile <bradford.haile@gmail.com>
2025-02-26 13:49:37 -0700

    vercel

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,16 @@
-import { MyNFTAbi } from './contracts/abi'; 
+import './styles/index.css'
+import { createPublicClient, http, decodeEventLog } from 'viem';
+import { evmFlowMainnet, evmFlowTestnet } from './lib/chains';
+import { 
+  connectWallet, 
+  handleAccountChange, 
+  getCurrentAccount,
+  getCurrentNetwork,
+  getWalletClient,
+  initAccountListeners,
+  CHAIN_ID_TO_NETWORK,
+  AccountState as State
+} from './lib/accounts';
+import { MyNFTAbi } from './contracts/abi';
+import deployedAddresses from './lib/deployed-addresses.json';
+import { fetchContractTransactions } from './lib/transactions'; 
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': resolve(__dirname, 'src')
     }
   }
 }); 
